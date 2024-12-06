f5382dba05ee9915ce4b0ec3dd51f872494ad6ae
sanskarchoudhry <sanskar22101@gmail.com>
2024-12-07 00:42:49 +0530

    configured typescript, created page component

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageProps } from "./types";
+
+const Page: React.FC<PageProps> = ({ title, children }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      {children}
+    </>
+  );
+};
+
+export default Page;
