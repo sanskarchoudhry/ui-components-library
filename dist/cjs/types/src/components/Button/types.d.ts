75eb465adfece7b0f93e0922749e2b1798676ee4
sanskarchoudhry <sanskar22101@gmail.com>
2025-01-03 18:17:55 +0530

    chore: added configuation for button styles

---
+++ dist/cjs/types/src/components/Button/types.d.ts
@@ -0,0 +1,7 @@
+import { type VariantProps } from "class-variance-authority";
+export declare const buttonVariants: (props?: ({
+    variant?: "default" | "accent" | "destructive" | "outline" | "secondary" | "ghost" | "link" | null | undefined;
+    size?: "default" | "sm" | "lg" | "icon" | null | undefined;
+} & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+}
